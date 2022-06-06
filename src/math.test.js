11708c35e6078d0afa5600c16efa0bc9a465df78
Nhật Vũ <nhatvu148@gmail.com>
2022-06-06 16:39:06 +0900

    different scenarios

--- src/math.test.js
+++ src/math.test.js
@@ -12,3 +12,24 @@ it("should summarize all number values in an array", () => {
   const expectedResult = numbers.reduce((prev, cur) => prev + cur, 0);
   expect(result).toBe(expectedResult);
 });
+
+it("should yield NaN if at least one invalid number is provided", () => {
+  const inputs = ["invalid", 1];
+
+  const result = add(inputs);
+
+  expect(result).toBeNaN();
+});
+
+it("should yield a correct sum if an array of numeric string values is provided", () => {
+    // Arrange
+    const numbers = ["1", "2"];
+  
+    // Act
+    const result = add(numbers);
+  
+    // Assert
+    const expectedResult = numbers.reduce((prev, cur) => +prev + +cur, 0);
+    expect(result).toBe(expectedResult);
+  });
+  
