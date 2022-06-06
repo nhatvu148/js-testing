d6ecf269b2b7f77c8585ac15e6a2ae2c98b1eaf2
Nhật Vũ <nhatvu148@gmail.com>
2022-06-06 17:02:20 +0900

    add more tests

--- src/math.test.js
+++ src/math.test.js
@@ -22,14 +22,37 @@ it("should yield NaN if at least one invalid number is provided", () => {
 });
 
 it("should yield a correct sum if an array of numeric string values is provided", () => {
-    // Arrange
-    const numbers = ["1", "2"];
-  
-    // Act
-    const result = add(numbers);
-  
-    // Assert
-    const expectedResult = numbers.reduce((prev, cur) => +prev + +cur, 0);
-    expect(result).toBe(expectedResult);
-  });
-  
+  const numbers = ["1", "2"];
+
+  const result = add(numbers);
+
+  const expectedResult = numbers.reduce((prev, cur) => +prev + +cur, 0);
+  expect(result).toBe(expectedResult);
+});
+
+it("should yield 0 if an empty array is provided", () => {
+  const numbers = [];
+
+  const result = add(numbers);
+
+  expect(result).toBe(0);
+});
+
+it("should throw an error if no value is passed into the function", () => {
+  const resultFn = () => {
+    add();
+  };
+
+  expect(resultFn).toThrow();
+});
+
+it("should throw an error if provided with multiple arguments instead of an array", () => {
+  const num1 = 1;
+  const num2 = 2;
+
+  const resultFn = () => {
+    add(num1, num2);
+  };
+
+  expect(resultFn).toThrow();
+});
