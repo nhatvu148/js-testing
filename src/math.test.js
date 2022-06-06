e281a5e62ed87b1e6894e4c82c95bbf2bb0fa4eb
Nhật Vũ <nhatvu148@gmail.com>
2022-06-06 16:32:59 +0900

    arrange, act, assert

--- src/math.test.js
+++ src/math.test.js
@@ -2,6 +2,13 @@ import { it, expect } from "vitest";
 import { add } from "./math";
 
 it("should summarize all number values in an array", () => {
-  const result = add([1, 2, 3]);
-  expect(result).toBe(6);
+  // Arrange
+  const numbers = [1, 2];
+
+  // Act
+  const result = add(numbers);
+
+  // Assert
+  const expectedResult = numbers.reduce((prev, cur) => prev + cur, 0);
+  expect(result).toBe(expectedResult);
 });
